069bc251bef342488437e68f43170bf31e46c51c
Vrbko <rvrbnjak@gmail.com>
2022-05-14 11:14:46 +0200

    POST na streznik v aplikaciji

--- NPO/app/src/main/java/com/example/projektnpo/MainActivity.kt
+++ NPO/app/src/main/java/com/example/projektnpo/MainActivity.kt
@@ -7,16 +7,23 @@ import android.hardware.SensorEventListener
 import android.hardware.SensorManager
 import android.location.Location
 import android.location.LocationRequest
+import android.os.Build
 import android.os.Bundle
+import android.os.StrictMode
+import android.os.StrictMode.ThreadPolicy
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 import com.example.projektnpo.databinding.ActivityMainBinding
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationCallback
 import com.google.android.gms.location.LocationServices
 
-class MainActivity : AppCompatActivity(), SensorEventListener {
 
+class MainActivity : AppCompatActivity(), SensorEventListener {
+    var serverResponse = ""
     lateinit var sensorManager: SensorManager
     lateinit var accelerometer: Sensor
     lateinit var gyroscope: Sensor
@@ -48,6 +55,11 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
         accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION)
         gyroscope = sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE)
 
+
+        if (Build.VERSION.SDK_INT > 9) {
+            val policy = ThreadPolicy.Builder().permitAll().build()
+            StrictMode.setThreadPolicy(policy)
+        }
     }
 
     override fun onPause() {
@@ -85,11 +97,66 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
             binding.textView5.text = y.toString()
             binding.textView6.text = z.toString()
         }
-
+        postData("xd");
     }
+
 
     override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
         return
     }
 
+
+     fun postData(text: String) {
+         val queue = Volley.newRequestQueue(this)
+         val url = "http://92.37.53.18:8080/cesta"
+//GET
+/*
+// Request a string response from the provided URL.
+         val stringRequest = StringRequest(Request.Method.GET, url,
+             Response.Listener<String> { response ->
+                 // Display the first 500 characters of the response string.
+                 Log.d(
+                     "VREI",
+                     "Response is: ${response.substring(0, 500)}"
+                 )
+             },
+             Response.ErrorListener {  Log.d(
+                 "VREI",
+                 "no"
+             ) })
+
+// Add the request to the RequestQueue.
+         queue.add(stringRequest)*/
+
+
+
+
+         // POST
+        val stringRequest: StringRequest = object : StringRequest(
+             Method.POST, url,
+             Response.Listener { response -> Log.d(
+                 "VREI",
+                 "Response is: ${response.substring(0, 42)}"
+             ) },
+             Response.ErrorListener {  Log.d(
+                 "VREI",
+                 "That didnt wor"
+             ) }) {
+             override fun getParams(): kotlin.collections.Map<String, String> {
+                 val paramV: MutableMap<String, String> = HashMap()
+                 paramV["user_id"] = "Vrbko";
+                 paramV["Latitude"] = "123";
+                 paramV["Longitude"] = "321";
+                 paramV["Altitude"] = "55";
+                 return paramV
+             }
+         }
+         queue.add(stringRequest)
+
+
+
+
+
+
+    }
 }
